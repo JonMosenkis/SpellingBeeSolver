8397ee716aa93344bebd0fb76ff44b49f4efcbd9
JonMosenkis <jonathanmos@wix.com>
2022-11-19 16:46:05 +0200

    Scrabble dictionary and relative import

--- src/main/scala/SpellingBee.scala
+++ src/main/scala/SpellingBee.scala
@@ -3,18 +3,16 @@ import trie.Trie
 import scala.io.Source
 
 object SpellingBee extends App {
-  val source = "/Users/jonathanmos/WixCode/SpellingBee/src/main/scala/utilities/words.txt"
-
   def getWordsFromFile: Seq[String] = {
     def isWord(string: String): Option[String] =
-      if (string.matches("^[a-z]+$")) Some(string.strip())
+      if (string.matches("^[a-zA-Z]+$")) Some(string.strip())
       else None
 
-    val bufferedSource = Source.fromFile(source)
+    val bufferedSource = Source.fromFile("src/main/resources/words.txt")
     val buffered = for {
       line <- bufferedSource.getLines()
       word <- isWord(line)
-    } yield word
+    } yield word.toLowerCase()
 
     val res = buffered.toList
     bufferedSource.close()
